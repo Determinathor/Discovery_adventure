21cdda1de978fb52671a3bc7a715c4cfa7d4888c
Spittyy <godthepro@gmail.com>
2024-06-24 21:13:54 +0200

    Admin.py changes

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from viewer.models import *
 # Register your models here.
+
+admin.site.register(Role)
+admin.site.register(User)
+admin.site.register(Manufacturer)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Order_Line)
+admin.site.register(Payment)
+
+
