eb3d03d6fd97b473e6759040947421104848d8f9
Spittyy <godthepro@gmail.com>
2024-06-24 19:01:21 +0200

    Vytvoreni homepage v url, zalozeni vieweru

--- discovery_adventure/urls.py
+++ discovery_adventure/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from viewer import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    #path('', home, name='home'),
+    path('hello/', views.hello, name='hello'),
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from viewer.templates import *
 
+from django.forms import *
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
+
+def hello(request):
+    return render(request, "hello.html")
