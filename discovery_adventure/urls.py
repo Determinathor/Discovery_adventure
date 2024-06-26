3b558ac4334b98216c2a00101fc00e48aab6eacb
Nabukadne <dandys96@seznam.cz>
2024-06-26 18:16:44 +0200

    Basic user management - Profile model, views for Login, SignUp, Password, urls.

--- discovery_adventure/urls.py
+++ discovery_adventure/urls.py
@@ -1,27 +1,30 @@
-"""discovery_adventure URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from discovery_adventure import settings
 
 from viewer import views
+from accounts.views import SubmittableLoginView, SignUpView, SubmittablePasswordChangeView
 
-from viewer.views import home
+from viewer.views import home, ProductsListView, ProductTemplateView, ProductCreateView, CategoryListView, \
+    CategoryTemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
+
     path('hello/', views.hello, name='hello'),
+
+    path('', CategoryListView.as_view(), name='home'), # "domovská stránka", která zobrazuje kategorie
+
+    # path('index/', CategoryListView.as_view(), name='index'), # "domovská stránka", která zobrazuje kategorie
+    path('shop/', ProductsListView.as_view(), name='shop'), # zobrazení všech produktů
+    path('category/<pk>/', CategoryTemplateView.as_view(), name='category'), # zobrazení produktů dané kategorie
+    path('product/<pk>', ProductTemplateView.as_view(), name='product'), # zobrazení detailu produktu
+    path('product/create/', ProductCreateView.as_view(), name='product_create'), # vytvoření produktu
+
+    path('accounts/login/', SubmittableLoginView.as_view(), name='login'),  # vlastní view pro login
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),          # vlastní view pro signup
+    path('accounts/password_change/', SubmittablePasswordChangeView.as_view(), name='password_change'), # view pro změnu hesla
+    path('accounts/', include('django.contrib.auth.urls')), # default django view
+
+    path('admin/', admin.site.urls),
 ]
