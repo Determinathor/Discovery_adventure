ba0cc614d96860907fda6986b7ba311920583ab0
Nabukadne <dandys96@seznam.cz>
2024-07-24 19:14:57 +0200

    fix settings

--- discovery_adventure/settings.py
+++ discovery_adventure/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'discovery_adventure.context_processors.cart_items_count',
-                'discovery_adventure.context_processors.search_results'
+                'discovery_adventure.context_processors.search_results',
                 'discovery_adventure.context_processors.cart_status',
             ],
         },
